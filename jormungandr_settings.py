6a7cbc7f452b8d024174f721c6cacc2b2583abe9
Vincent Lepot <vincent.lepot@canaltp.fr>
2016-04-11 14:51:53 +0200

    First commit

---
+++ jormungandr_settings.py
@@ -0,0 +1,93 @@
+# encoding: utf-8
+
+import logging
+
+# path of the configuration file for each instances
+INSTANCES_DIR = '/etc/jormungandr.d'
+
+# Start the thread at startup, True in production, False for test environments
+START_MONITORING_THREAD = True
+
+#URI for postgresql
+# postgresql://<user>:<password>@<host>:<port>/<dbname>
+#http://docs.sqlalchemy.org/en/rel_0_9/dialects/postgresql.html#psycopg2
+SQLALCHEMY_DATABASE_URI = 'postgresql://navitia:navitia@database/jormungandr'
+
+DISABLE_DATABASE=True
+
+# disable authentication
+PUBLIC = True
+
+#message returned on authentication request
+HTTP_BASIC_AUTH_REALM = 'Token Required'
+
+from jormungandr.logging_utils import IdFilter
+
+# logger configuration
+LOGGER = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters':{
+        'default': {
+            'format': '[%(asctime)s] [%(request_id)s] [%(levelname)5s] [%(process)5s] [%(name)10s] %(message)s',
+        },
+    },
+    'filters': {
+        'IdFilter': {
+            '()': IdFilter,
+        }
+    },
+    'handlers': {
+        'default': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'default',
+            'filters': ['IdFilter'],
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['default'],
+            'level': 'DEBUG',
+            'propagate': True
+        },
+    }
+}
+
+#Parameters for statistics
+SAVE_STAT = False
+BROKER_URL = 'amqp://guest:guest@rabbitmq:5672//'
+EXCHANGE_NAME = 'stat_persistor_exchange'
+
+#Cache configuration, see https://pythonhosted.org/Flask-Cache/ for more information
+CACHE_CONFIGURATION = {
+    'CACHE_TYPE': 'null',  # by default cache is not activated
+    'TIMEOUT_PTOBJECTS': 600,
+    'TIMEOUT_AUTHENTICATION': 600,
+    'TIMEOUT_PARAMS': 600,
+    'TIMEOUT_TIMEO': 60,
+    'TIMEOUT_SYNTHESE': 30,
+}
+
+# List of enabled modules
+MODULES = {
+    'v1': {  # API v1 of Navitia
+        'import_path': 'jormungandr.modules.v1_routing.v1_routing',
+        'class_name': 'V1Routing'
+    }
+}
+
+AUTOCOMPLETE = None
+
+# circuit breaker parameters.
+CIRCUIT_BREAKER_MAX_INSTANCE_FAIL = 4  # max instance call failures before stopping attempt
+CIRCUIT_BREAKER_INSTANCE_TIMEOUT_S = 60  # the circuit breaker retries after this timeout (in seconds)
+
+CIRCUIT_BREAKER_MAX_TIMEO_FAIL = 4  # max instance call failures before stopping attempt
+CIRCUIT_BREAKER_TIMEO_TIMEOUT_S = 60  # the circuit breaker retries after this timeout (in seconds)
+
+CIRCUIT_BREAKER_MAX_SYNTHESE_FAIL = 4  # max instance call failures before stopping attempt
+CIRCUIT_BREAKER_SYNTHESE_TIMEOUT_S = 60  # the circuit breaker retries after this timeout (in seconds)
+
+# Default region instance
+# DEFAULT_REGION = 'default'
---
+++ tyr_settings.py
@@ -0,0 +1,118 @@
+#encoding: utf-8
+import logging
+from datetime import timedelta
+
+#URL for the brokker, by default it's the local rabbitmq
+#For amqp (rabbitMQ) the syntax is:
+#amqp://<user>:<password>@<host>:<port>/<vhost>
+#the default vhost is "/" so the URL end with *two* slash
+#http://docs.celeryproject.org/en/latest/configuration.html#std:setting-BROKER_URL
+CELERY_BROKER_URL = 'amqp://guest:guest@rabbitmq:5672//'
+
+#URI for postgresql
+# postgresql://<user>:<password>@<host>:<port>/<dbname>
+#http://docs.sqlalchemy.org/en/rel_0_9/dialects/postgresql.html#psycopg2
+SQLALCHEMY_DATABASE_URI = 'postgresql://navitia:navitia@database/jormungandr'
+
+
+#URI for cities database
+# postgresql://<user>:<password>@<host>:<port>/<dbname>
+#http://docs.sqlalchemy.org/en/rel_0_9/dialects/postgresql.html#psycopg2
+CITIES_DATABASE_URI = 'postgresql://navitia:navitia@database/cities'
+
+
+#Path to the directory where the configuration file of each instance of ed are defined
+INSTANCES_DIR = '.'
+
+#Log Level available
+# - DEBUG
+# - INFO
+# - WARN
+# - ERROR
+
+# logger configuration
+LOGGER = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s] [%(levelname)5s] [%(process)5s] [%(name)25s] %(message)s',
+        },
+        'instance': {
+            'format': '%(name)s: [%(asctime)s] [%(levelname)5s] [%(process)5s] %(message)s',
+        }
+    },
+    'handlers': {
+        'default': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'default',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['default'],
+            'level': 'DEBUG',
+        },
+        'celery':{
+            'level': 'INFO',
+        },
+        'sqlalchemy.engine': {
+            'handlers': ['default'],
+            'level': 'WARN',
+            'propagate': True
+        },
+        'sqlalchemy.pool': {
+            'handlers': ['default'],
+            'level': 'WARN',
+            'propagate': True
+        },
+        'sqlalchemy.dialects.postgresql': {
+            'handlers': ['default'],
+            'level': 'WARN',
+            'propagate': True
+        },
+    }
+}
+
+REDIS_HOST = 'redis'
+
+REDIS_PORT = 6379
+
+#index of the database use in redis, between 0 and 15 by default
+REDIS_DB = 0
+
+REDIS_PASSWORD = None
+
+#Validate the presence of a mx record on the domain
+EMAIL_CHECK_MX = True
+
+#Validate the email by connecting to the smtp server, but doesn't send an email
+EMAIL_CHECK_SMTP = True
+
+#configuration of celery, don't edit
+CELERY_ACCEPT_CONTENT = ['pickle', 'json']
+
+CELERYBEAT_SCHEDULE = {
+    'udpate-data-every-30-seconds': {
+        'task': 'tyr.tasks.update_data',
+        'schedule': timedelta(seconds=30),
+        'options': {'expires': 25}
+    },
+    'scan-instances-every-minutes': {
+        'task': 'tyr.tasks.scan_instances',
+        'schedule': timedelta(minutes=1),
+        'options': {'expires': 50}
+    },
+    'heartbeat-kraken': {
+        'task': 'tyr.tasks.heartbeat',
+        'schedule': timedelta(minutes=30),
+        'options': {'expires': 50}
+    },
+}
+CELERY_TIMEZONE = 'UTC'
+
+#http://docs.celeryproject.org/en/master/configuration.html#std:setting-CELERYBEAT_SCHEDULE_FILENAME
+CELERYBEAT_SCHEDULE_FILENAME = '/tmp/celerybeat-schedule'
+
+CELERYD_HIJACK_ROOT_LOGGER = False
